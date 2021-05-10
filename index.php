481f61e03e2dcdaa3a814124e47df1748353fefc
Valdir <toddyabc@gmail.com>
2021-05-10 18:43:34 -0300

    pequenos ajustes

--- index.php
+++ index.php
@@ -3,19 +3,21 @@
 require_once("vendor/autoload.php");
 
 //require_once("hcodebr/php-classes/src/DB/Sql.php");
-use Hcode\DB\Sql;
-//$app = new \Slim\Slim();
+use \Slim\Slim;
+use \Hcode\Page;
+//use Hcode\DB\Sql;
+$app = new Slim();
 
-//$app->config('debug', true);
+$app->config('debug', true);
 
-//$app->get('/', function() {
+$app->get('/', function() {
 
-	$sql = new Sql();
-	$results = $sql->select("SELECT * FROM tb_users");
-	echo json_encode($results);
+	$page = new Page(); //Ao criar o objeto o construct cria o header.
+	$page->setTpl("index"); //Aqui o arquivo index é incluido. Ele contém o corpo do template.
+// Por fim o destruct limpa a memória do processador e insere o footer.
 
-//});
+});
 
-//$app->run();
+$app->run();
 
  ?>
